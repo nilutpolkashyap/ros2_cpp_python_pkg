1a6e88309519e9f2cc90f89970d70bacdfc6da0c
nilutpolkashyap <nkelectronicshlnp@gmail.com>
2023-04-14 18:31:01 +0530

    parameters in ROS 2

--- src/rpm_publisher.cpp
+++ src/rpm_publisher.cpp
@@ -5,7 +5,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/float64.hpp"
 
-const float RPM_VALUE = 10.0;
+const float RPM_DEFAULT_VALUE = 10.0;
 
 using namespace std::chrono_literals;
 
@@ -14,6 +14,7 @@ class RpmPubNode : public rclcpp::Node
   public: 
     RpmPubNode() : Node("rpm_publisher_node") 
     {
+      this->declare_parameter<double>("rpm_val", RPM_DEFAULT_VALUE);
       publisher_ = this->create_publisher<std_msgs::msg::Float64>("rpm", 10);
       timer_ = this->create_wall_timer(1s, std::bind(&RpmPubNode::publish_rpm, this));
       std::cout << "RPM Publisher Node Is Running..." << std:: endl;
@@ -23,13 +24,17 @@ class RpmPubNode : public rclcpp::Node
     void publish_rpm()
     {
       auto message = std_msgs::msg::Float64();
-      message.data = RPM_VALUE;
+      this->get_parameter("rpm_val", rpm_param_val_);
+      message.data = rpm
+      // message.data = RPM_VALUE;
+
       RCLCPP_INFO(this->get_logger(), "Published RPM: '%f'", message.data);
       publisher_->publish(message);
     }
 
     rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr publisher_;
     rclcpp::TimerBase::SharedPtr timer_;
+    double rpm_param_val_ = RPM_DEFAULT_VALUE;
 };
 
 
